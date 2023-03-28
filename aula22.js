d0365bd78d6f1bb55f404f15c030af2cd6122743
Eduardo Lago <eduardolagonunes@gmail.com>
2023-03-28 15:54:18 -0300

    função e função parametrizada

---
+++ aula22.js
@@ -0,0 +1,41 @@
+// EX 1
+/* function soma(n1=0, n2=0){
+    console.log(n1 + n2)
+}
+
+soma(1, 2)
+
+function soma(n1=0, n2=0){
+    console.log(n1 + n2)
+} 
+
+soma(1, 2)*/
+
+
+
+// EX 2
+/* function soma(n1=valor_padrao, n2=valor_padrao){
+    let res
+    res=n1+n2
+    return res
+}
+
+let resultado_soma=soma(5)
+console.log(resultado_soma) */
+
+// EX 3
+const valor_padrao=0
+
+
+
+function add(v){
+    return valor+v
+}
+
+let valor=0
+
+add(10)
+console.log(valor)
+
+add(5)
+console.log(valor)
